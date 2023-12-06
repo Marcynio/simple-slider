3eb1da695fdecb813be848e0273019cfcdbeed37
Marcin Cydejko <marcin.cydejko98@gmail.com>
2023-12-06 11:47:34 +0100

    git commit

---
+++ main1.js
@@ -0,0 +1,17 @@
+const slider = document.querySelector(".slider");
+const slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function updateSlider() {
+  slider.style.transform = `translateX(${-index * 100}%)`;
+}
+
+function nextSlide() {
+  index = (index + 1) % slides.length;
+  updateSlider();
+}
+
+function prevSlide() {
+  index = (index - 1 + slides.length) % slides.length;
+  updateSlider();
+}
